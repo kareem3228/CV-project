8fda0e2da273ba583a4c2df94c852080e8537a32
kareem3228 <kareemmagdy9791@gmail.com>
2025-05-11 03:41:26 +0300

    project.py

--- Programming/Python_scripts/project.py
+++ Programming/Python_scripts/project.py
@@ -4,6 +4,9 @@ from fer import FER
 detector = FER(mtcnn=True)  # use MTCNN for face detection
 cap = cv2.VideoCapture(0)
 
+# forimages
+# frame= cv2.imread(r"C:\Users\karee\OneDrive\Pictures\Screenshots\Screenshot 2025-05-10 212750.png")
+
 while True:
     ret, frame = cap.read()
     if not ret: 
@@ -11,12 +14,12 @@ while True:
 
     # FER detector returns a list of faces with emotion scores
     results = detector.detect_emotions(frame)
-    
-    
+
+
     for face in results:
         x, y, w, h = face["box"]
         emotions = face["emotions"]    # dict of emotion scores
-        if emotions["disgust"] > 0.01:
+        if emotions["disgust"] > 0.005:
             emotions["disgust"] *= 20
             emotions["fear"] = 0
             emotions["sad"] = 0
@@ -25,13 +28,10 @@ while True:
             emotions["neutral"] = 0
         else:
             emotions["fear"] *= 1.5
-            emotions["sad"] *= 2.3
-            emotions["angry"] *= 1.5
+            emotions["sad"] *= 2
+            emotions["angry"] *= 2.2
             emotions['happy'] *=0.5
             emotions["neutral"] *= 0.5
-        # Normalize the scores to sum to 1
-        total = sum(emotions.values())
-        emotions = {k: v / total for k, v in emotions.items()}
         # Pick the emotion with highest score
         dominant_emotion = max(emotions, key=emotions.get)
 
@@ -43,6 +43,8 @@ while True:
     cv2.imshow("FER Emotion", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+#for images
+# cv2.waitKey(0)
 
 cap.release()
 cv2.destroyAllWindows()
